0f9c4fd4ae6eafae9939513d5348ae298c756454
nobody.jbj <nobody.jbj@gmail.com>
2021-02-18 19:06:42 +0900

    security working..

--- src/main/java/com/api/auth/config/SecurityConfig.java
+++ src/main/java/com/api/auth/config/SecurityConfig.java
@@ -1,4 +1,22 @@
 package com.api.auth.config;
 
-public class SecurityConfig {
+import com.api.auth.accounts.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
 }
